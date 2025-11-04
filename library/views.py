859b86b9d29c227a02580b0316242b571875df86
Kolade Bakare <bakarekolade@ymail.com>
2025-11-04 20:02:38 +0100

    Extend Date endpoint

--- library/views.py
+++ library/views.py
@@ -4,7 +4,8 @@ from .models import Author, Book, Member, Loan
 from .serializers import AuthorSerializer, BookSerializer, MemberSerializer, LoanSerializer
 from rest_framework.decorators import action
 from django.utils import timezone
-from .tasks import send_loan_notification
+from datetime import timedelta
+from .tasks import send_loan_notification, check_overdue_loans
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
@@ -52,3 +53,32 @@ class MemberViewSet(viewsets.ModelViewSet):
 class LoanViewSet(viewsets.ModelViewSet):
     queryset = Loan.objects.all()
     serializer_class = LoanSerializer
+
+    @action(detail=True, method=['post'])
+    def extend_due_date(self, request, pk=None):
+        try:
+            loan = Loan.objects.select_related('member__user', 'book').get(pk=pk)
+        except Loan.DoesNotExist:
+            return Response({'error': 'Loan not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+        today = timezone.now().date()
+        if loan.is_returned:
+            return Response({'error': 'Loaned book returned already.'}, status=status.HTTP_204_NO_CONTENT)
+        if loan.due_date < today:
+            return Response({'error': 'Loan is overdue and cannot be extended.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        additional_days = request.data.get('additional_days')
+        if additional_days is None:
+            return Response({'error': 'Additional days is required'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            additional_days = int(additional_days)
+        except (TypeError, ValueError):
+            return Response({'error': 'Additional days must be positive integer'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if additional_days <= 0:
+            return Response({'error': 'Additional days must be positive integer'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        loan.due_date = loan.due_date + timedelta(days=additional_days)
+        loan.save(update_fields=['due_date'])
+
+        return Response(LoanSerializer(loan).data, status=status.HTTP_200_OK)
